2e3769d10f0c179042d55bcdb8d711075727506f
Chema Durán <prof.jduran@iesalixar.org>
2020-11-05 10:29:26 +0100

    añadimos script de update de alumnos

---
+++ update_repo_alumnos.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+git --git-dir=alumnos/abraham_ruiz/.git checkout master 
+git --git-dir=alumnos/alvaro_gil/.git checkout master 
+git --git-dir=alumnos/daniel_real/.git checkout master 
+git --git-dir=alumnos/david_f_aliseda/.git checkout master 
+git --git-dir=alumnos/gabriel_karajallo/.git checkout master 
+git --git-dir=alumnos/isidro_perez/.git checkout master 
+git --git-dir=alumnos/juancarlos_rayo/.git checkout master 
+git --git-dir=alumnos/juanlu_hernandez/.git checkout master 
+git --git-dir=alumnos/julian_alvarez/.git checkout master 
+git --git-dir=alumnos/luis_arroyo/.git checkout main 
+git --git-dir=alumnos/manuel_colchero/.git checkout main
+git --git-dir=alumnos/miguel_castro/.git checkout main 
+git --git-dir=alumnos/oscar_alcala/.git checkout master 
+git --git-dir=alumnos/pablo_sanchez/.git checkout master 
+git --git-dir=alumnos/rafael_gomez/.git checkout master 
+git --git-dir=alumnos/rafael_ortega/.git checkout master 
+git --git-dir=alumnos/ricardo_chimborazo/.git checkout main 
+git --git-dir=alumnos/sebastian_bermudez/.git checkout master 
+
+git --git-dir=alumnos/abraham_ruiz/.git pull
+git --git-dir=alumnos/alvaro_gil/.git pull 
+git --git-dir=alumnos/daniel_real/.git pull
+git --git-dir=alumnos/david_f_aliseda/.git pull 
+git --git-dir=alumnos/gabriel_karajallo/.git pull 
+git --git-dir=alumnos/isidro_perez/.git pull 
+git --git-dir=alumnos/juancarlos_rayo/.git pull 
+git --git-dir=alumnos/juanlu_hernandez/.git pull 
+git --git-dir=alumnos/julian_alvarez/.git pull 
+git --git-dir=alumnos/luis_arroyo/.git pull 
+git --git-dir=alumnos/manuel_colchero/.git pull 
+git --git-dir=alumnos/miguel_castro/.git pull 
+git --git-dir=alumnos/oscar_alcala/.git pull 
+git --git-dir=alumnos/pablo_sanchez/.git pull 
+git --git-dir=alumnos/rafael_gomez/.git pull 
+git --git-dir=alumnos/rafael_ortega/.git pull 
+git --git-dir=alumnos/ricardo_chimborazo/.git pull 
+git --git-dir=alumnos/sebastian_bermudez/.git pull 
